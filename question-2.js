cf88c82b640a0114c961c62d297914dcf8e837ff
k12re <83732570+k12re@users.noreply.github.com>
2022-08-24 18:12:28 +0200

    Did q1- and almost done with q2.

---
+++ question-2.js
@@ -0,0 +1,31 @@
+//Question 2
+
+const key = "57c27ce3640540d3b4d65f5802d59b7c";
+const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`;
+
+const resultsContainer = document.querySelector(".container");
+
+async function APIcall() {
+  try {
+    const response = await fetch(url);
+    const { results } = await response.json();
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i <= 7; i++) {
+      //console.log("Name: ", results[i].name, "Rating: ", results[i].rating, "Number of tags: ", results[i].tags.length);
+
+      resultsContainer.innerHTML += `<div class="result">
+      <h3>Name: ${results[i].name}</h3>
+      <p>Rating: ${results[i].rating}</p>
+      <p>Number of tags: ${results[i].tags.length}</p>
+      </div>
+      `;
+    }
+  } catch (error) {
+    resultsContainer.innerHTML = `<div class="error">An error occurred</div>`;
+    //console.log("An error occurred");
+  }
+}
+
+APIcall();
